af68b2a5a08bdbb4a263939267f45e55359a7b86
hui.wang <hui.wang@ifchange.com>
2019-06-25 18:11:55 +0800

    .

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/tomnattle/gof
 
 go 1.12
+
+require github.com/hsyan2008/hfw2 v0.0.0-20190612091722-19e0f0a6807f // indirect
--- gof.go
+++ gof.go
@@ -11,12 +11,17 @@ func New() *Gof {
 
 type Request interface {
 }
+
 type Response interface {
 }
 
 type RequestHandler func(*context.Context, *Request, *Response) (err error)
 
 func (g *Gof) Router(path string, handler RequestHandler) {
+
+}
+
+func (g *Gof) Get(path string, handler RequestHandler) {
 
 }
 
