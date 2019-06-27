dc5103d83e5925da51ca4544811a974f0bbd5b43
hui.wang <hui.wang@ifchange.com>
2019-06-27 22:55:16 +0800

    .

--- cms/main.go
+++ cms/main.go
@@ -3,10 +3,12 @@ package main
 import (
 	"github.com/tomnattle/gof"
 	"github.com/tomnattle/gof/cms/controller/article"
+	"github.com/tomnattle/gof/module/log"
 )
 
 func main() {
 	app := gof.New()
+	app.SetLogger(log.NewTomLogger())
 	app.Rest("/article", &article.Article{})
 	app.Start()
 }
